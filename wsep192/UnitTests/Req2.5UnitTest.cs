acaa4a15d4f046ce600f322787684f49b11fe54d
Aviv Maimon <maimonav@post.bgu.ac.il>
2019-04-03 17:49:02 +0300

    ...

--- wsep192/UnitTests/Req2.5UnitTest.cs
+++ wsep192/UnitTests/Req2.5UnitTest.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
-    /// <summary>
-    /// Summary description for Req2
-    /// </summary>
+    
     [TestClass]
     public class Req2
     {
-   
+        
         [TestMethod]
         public void TestMethod1()
         {
