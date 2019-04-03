633ff8d4c81b6628273118e107669c23e8f3d623
Aviv Maimon <maimonav@post.bgu.ac.il>
2019-04-03 13:31:29 +0300

    all contructors and field were updated.

--- wsep192/src/Domain/Filter.cs
+++ wsep192/src/Domain/Filter.cs
@@ -11,5 +11,22 @@ namespace src.Domain
         private KeyValuePair <int, int> priceRange;
         private int productRate;
         private int storeRate;
+
+        public Filter(string productName, string category, string keyword, KeyValuePair<int, int> priceRange, int productRate, int storeRate)
+        {
+            this.ProductName = productName;
+            this.Category = category;
+            this.Keyword = keyword;
+            this.PriceRange = priceRange;
+            this.ProductRate = productRate;
+            this.StoreRate = storeRate;
+        }
+
+        public string ProductName { get => productName; set => productName = value; }
+        public string Category { get => category; set => category = value; }
+        public string Keyword { get => keyword; set => keyword = value; }
+        public KeyValuePair<int, int> PriceRange { get => priceRange; set => priceRange = value; }
+        public int ProductRate { get => productRate; set => productRate = value; }
+        public int StoreRate { get => storeRate; set => storeRate = value; }
     }
 }
