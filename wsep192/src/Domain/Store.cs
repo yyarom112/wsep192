b95a022eee790dd3409207375ff203ec226d259c
yyarom112 <yaromy@post.bgu.ac.il>
2019-04-03 13:35:03 +0300

    add itree type

--- wsep192/src/Domain/Store.cs
+++ wsep192/src/Domain/Store.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Common;
+
 namespace src.Domain
 {
     class Store
@@ -12,7 +14,7 @@ namespace src.Domain
         private String name;
         private Dictionary<int, ProductInStore> products;
         private int storeRate;
-        private Tree<Role> roles;
+        private ITree<Role> roles;
         private List<PurchasePolicy> purchasePolicy;
         private List<DiscountPolicy> discountPolicy;
 
