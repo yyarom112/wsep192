2327dc7f8ff4ac47395dd041a6b507671d349436
Aviv Maimon <maimonav@post.bgu.ac.il>
2019-04-04 14:38:25 +0300

    few updates in testunit

---
+++ wsep192/UnitTests/Req4.4UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.Domain;
+
+
+namespace UnitTests
+{
+
+    [TestClass]
+    public class Req4
+    {
+        private src.Domain.System sys;
+        public void buildMocks()
+        {
+           
+
+        }
+        [TestMethod]
+        public void TestRemoveOwner()
+        {
+
+
+        }
+    }
+}
