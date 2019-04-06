d570b00cd5160cabd083d041d4a78eb15b7d4fad
Seifang <seifang@post.bgu.ac.il>
2019-04-06 13:06:04 +0300

    before doing merge from master

--- wsep192/UnitTests/assignManagerTest.cs
+++ wsep192/UnitTests/assignManagerTest.cs
@@ -1,69 +1,31 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.Domain;
 
 namespace UnitTests
 {
-    /// <summary>
-    /// Summary description for assignManagerTest
-    /// </summary>
+   
     [TestClass]
     public class assignManagerTest
     {
-        public assignManagerTest()
+        private TradingSystem system;
+        private User ownerUser;
+        private User managerUser;
+        private Store store1;
+
+        public void setUp()
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            system = new TradingSystem(null, null);
+            ownerUser = new User(1234, "Seifan", "2457", true, true);
+            
+            system.Users.Add(ownerUser.Id, ownerUser);
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void TestMethod1()
         {
-            //
-            // TODO: Add test logic here
-            //
+            
         }
     }
 }
--- wsep192/src/Domain/User.cs
+++ wsep192/src/Domain/User.cs
@@ -19,17 +19,18 @@ namespace src.Domain
         private ShoppingBasket basket;
         private Dictionary<int, Role> roles;
 
-        public User(int id, string userName, string password, string address, state state, bool isAdmin, bool isRegistered)
+        public User(int id, string userName, string password, bool isAdmin, bool isRegistered)
         {
             this.id = id;
             this.userName = userName;
             this.password = password;
-            this.address = address;
-            this.state = state;
+            this.address = "";
+            this.state = state.visitor;
             this.isAdmin = isAdmin;
             this.isRegistered = isRegistered;
             this.basket = new ShoppingBasket();
             this.roles = new Dictionary<int, Role>();
+
         }
 
         public int Id { get => id; set => id = value; }
