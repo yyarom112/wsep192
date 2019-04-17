592dac029edd92a44adbd4670aba54807cb11055
rotem <rotem@DESKTOP-DR8HOV0>
2019-04-17 16:37:02 +0300

    acceptance tests

---
+++ wsep192/Acceptance Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.ServiceLayer;
+namespace Acceptance_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private ServiceLayer service;
+        private string user;
+
+        public void setUp()
+        {
+            service = new ServiceLayer();
+            service.init("admin", "1234");
+            user=service.initUser();
+        }
+
+        [TestMethod]
+        //an unregisterd user tries to preform open store.
+        public void testOpenStore1()
+        {
+            setUp();
+            bool x = service.openStore("Bershka", "Yonit Levy");
+            Assert.IsFalse(x);
+        }
+
+        [TestMethod]
+        //a registerd user tries to preform open store.
+        public void testOpenStore2()
+        {
+            setUp();
+            service.register("Yonit Levy", "23&As2", user);
+            bool x = service.openStore("Bershka", "Yonit Levy");
+            Assert.IsTrue(x);
+        }
+    }
+}
---
+++ wsep192/Acceptance Tests/removeUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.ServiceLayer;
+
+namespace Acceptance_Tests
+{
+    [TestClass]
+    public class removeUser
+    {
+        ServiceLayer service;
+        String idOwner;
+        String ownerUser;
+        String passwordUser;
+
+        public void setUp()
+        {
+            service = new ServiceLayer();
+            service.init("Admin", "2323");
+            idOwner = service.initUser();
+            ownerUser = "Seifan";
+            passwordUser = "2345";
+            service.register(ownerUser, passwordUser, idOwner);
+            service.signIn(ownerUser, passwordUser);
+        }
+
+        [TestMethod]
+        //The admin removes an existing user - valid
+        public void removeUserTest1()
+        {
+            setUp();
+            bool x=service.removeUser("2323", "2345");
+            Assert.IsTrue(x);
+        }
+
+        [TestMethod]
+        //Not the admin removes an existing user - invalid
+        public void removeUserTest2()
+        {
+            setUp();
+            bool x = service.removeUser("2323", "2345");
+            Assert.IsFalse(x);
+        }
+
+        //The admin removes a non existing user - invalid
+        public void removeUserTest3()
+        {
+            setUp();
+            bool x = service.removeUser("2323", "2345");
+            Assert.IsFalse(x);
+        }
+    }
+}
