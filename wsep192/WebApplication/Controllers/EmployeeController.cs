609371570ef42aecfe3735ccad5cd4b58d8f6e61
Aviv Maimon <maimonav@post.bgu.ac.il>
2019-05-02 14:51:53 +0300

    start of web app

---
+++ wsep192/WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Search(string name)
+        {
+            var input = Server.HtmlEncode(name);
+            return Content(input);
+        }
+    }
+}
