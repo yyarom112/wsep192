d19c05b7e296f35d4bf759fc96745b429d3f8a60
Maor Sagi <maos@post.bgu.ac.il>
2019-05-06 17:08:28 +0300

    start websockets

---
+++ wsep192/WebServices/Controllers/WebsocketsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebServices.Controllers
+{
+    public class WebsocketsController : ApiController
+    {
+        private Dictionary<int, WebSocket> webSockets = new Dictionary<int, WebSocket>(); 
+
+
+        // GET: Websockets
+        public string Test()
+        {
+            return "Hello!";
+        }
+    }
+}
