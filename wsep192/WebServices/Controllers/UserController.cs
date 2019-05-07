502b78a0524803ca3a3902feced456086fc44c0c
rotem <rotem@DESKTOP-DR8HOV0>
2019-05-07 18:52:39 +0300

    update

--- wsep192/WebServices/Controllers/UserController.cs
+++ wsep192/WebServices/Controllers/UserController.cs
@@ -126,5 +126,20 @@ namespace WebService.Controllers
             }
             return "Server error: assignManager";
         }
+
+        [Route("api/user/setUp")]
+        [HttpGet]
+        public string setUp()
+        {
+            bool ans = service.setUp();
+            switch (ans)
+            {
+                case true:
+                    return "System setup completed";
+                case false:
+                    return "Error in setUp";
+            }
+            return "Server error: setUp";
+        }
     }
 }
