f014dc1314fc8b772f581e2677ae437e714b78a7
yyarom112 <yaromy@post.bgu.ac.il>
2019-05-11 19:47:39 +0300

    Done- LeafCondition with unit test

--- wsep192/UnitTests/DiscountPolicyStructureTest.cs
+++ wsep192/UnitTests/DiscountPolicyStructureTest.cs
@@ -11,17 +11,33 @@ namespace UnitTests
         private RevealedDiscount rd;
 
         private Product p1;
-        private Store store;
         private ProductInStore pis1;
+
+        private Product p2;
+        private ProductInStore pis2;
+
+        private Store store;
+        private LeafCondition lc;
 
         public void sutup()
         {
-            p1 = new Product(0, "p1", null, null, 1);
+            p1 = new Product(0, "p1", null, null, 20);
             store = new Store(7, "a");
             pis1 = new ProductInStore(20, store, p1);
+
+            p2 = new Product(1, "p2", null, null, 20);
+            pis2 = new ProductInStore(20, store, p2);
+
+
             Dictionary<int, KeyValuePair<ProductInStore, int>> productList = new Dictionary<int, KeyValuePair<ProductInStore, int>>();
             productList.Add(p1.Id, new KeyValuePair<ProductInStore, int>(pis1, 1));
             rd = new RevealedDiscount(1, 0.5, productList, new DateTime(2222, 1, 1), DuplicatePolicy.WithMultiplication);
+
+
+            Dictionary<int, KeyValuePair<ProductInStore, int>> relatedProducts = new Dictionary<int, KeyValuePair<ProductInStore, int>>();
+            relatedProducts.Add(p1.Id, new KeyValuePair<ProductInStore, int>(pis1, 1));
+            relatedProducts.Add(p2.Id, new KeyValuePair<ProductInStore, int>(pis2, 2));
+            lc = new LeafCondition(relatedProducts);
         }
 
         //----------------------@@ Revealed Discount @@--------------------------
@@ -48,9 +64,30 @@ namespace UnitTests
             Assert.AreEqual(0, rd.calculate(productToBuy), "check not relevant product");
 
             productToBuy = new List<KeyValuePair<ProductInStore, int>>();
-            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis1, 2));
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis1, 1));
             Assert.AreEqual(10, rd.calculate(productToBuy));
 
         }
+
+        //----------------------@@ Leaf Condition @@--------------------------
+        [TestMethod]
+        public void LeafCondition_checkCondition()
+        {
+            sutup();
+            List<KeyValuePair<ProductInStore, int>> productToBuy = new List<KeyValuePair<ProductInStore, int>>();
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis1, 1));
+            Assert.AreEqual(false, lc.checkCondition(productToBuy), "Missing products related to the discount");
+            productToBuy = new List<KeyValuePair<ProductInStore, int>>();
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis1, 1));
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis2, 1));
+            Assert.AreEqual(false, lc.checkCondition(productToBuy), "pis2 does not exceed the minimum amount");
+            productToBuy = new List<KeyValuePair<ProductInStore, int>>();
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis1, 1));
+            productToBuy.Add(new KeyValuePair<ProductInStore, int>(pis2, 2));
+            Assert.AreEqual(true, lc.checkCondition(productToBuy), "The success scenario");
+
+
+        }
+
     }
 }
--- wsep192/src/Domain/LeafCondition.cs
+++ wsep192/src/Domain/LeafCondition.cs
@@ -8,11 +8,32 @@ namespace src.Domain
 {
     class LeafCondition : ConditionalDiscount
     {
-        private Dictionary<int, KeyValuePair<ProductInStore, int>> relatedProducts;
+        private Dictionary<int, KeyValuePair<ProductInStore, int>> relatedProducts; //<productID , <Product, minimum quntitiy to buy>>
 
+        public LeafCondition(Dictionary<int, KeyValuePair<ProductInStore, int>> relatedProducts)
+        {
+            this.relatedProducts = relatedProducts ?? throw new ArgumentNullException(nameof(relatedProducts));
+        }
+
+
+        //If any product related to the discount is listed in the appropriate quantity,
+        //you will return the true
         public override bool checkCondition(List<KeyValuePair<ProductInStore, int>> productList)
         {
-            throw new NotImplementedException();
+            int sumOfProducts = 0;
+            foreach (KeyValuePair<ProductInStore, int> product in productList)
+            {
+                //If the product exists in the list but not in the appropriate quantity will return a false
+                if (relatedProducts.ContainsKey(product.Key.Product.Id) && product.Value < this.relatedProducts[product.Key.Product.Id].Value)
+                    return false;
+                //If the product exists in the list will increase the amount of suitable products
+                if (relatedProducts.ContainsKey(product.Key.Product.Id))
+                    sumOfProducts++;
+            }
+            //If all products are in the correct quantity will return true
+            if (sumOfProducts== relatedProducts.Count)
+                return true;
+            return false;
         }
     }
 }
