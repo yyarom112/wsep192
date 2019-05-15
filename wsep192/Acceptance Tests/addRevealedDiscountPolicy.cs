d091b78fd789c56c47224c2022b8b0ebd2ee1869
Seifang <seifang@post.bgu.ac.il>
2019-05-15 16:48:19 +0300

    add acceptance test - not finish

---
+++ wsep192/Acceptance Tests/addRevealedDiscountPolicy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.ServiceLayer;
+using src.Domain;
+
+namespace Acceptance_Tests
+{
+    
+    [TestClass]
+    public class addRevealedDiscountPolicy
+    {
+        ServiceLayer service;
+        String idOwner;
+        String ownerUser;
+        String passwordUser;
+
+        List<KeyValuePair<String, int>> products;
+        Dictionary<int, KeyValuePair<ProductInStore, int>> productsInCart;
+
+        public void setUp()
+        {
+            service = ServiceLayer.getInstance();
+            idOwner = service.initUser();
+            ownerUser = "Seifan";
+            passwordUser = "2345";
+            service.register(ownerUser, passwordUser, idOwner);
+            service.signIn(ownerUser, passwordUser);
+
+            service.openStore("adidas", ownerUser);
+            service.createNewProductInStore("milk", "milk", "none", 10, "adidas", ownerUser);
+            products = new List<KeyValuePair<String, int>>();
+            products.Add(new KeyValuePair<string, int>("milk", 7));
+            service.addProductsInStore(products, "adidas", ownerUser);
+
+            productsInCart = new Dictionary<int, KeyValuePair<ProductInStore, int>>();
+            //productsInCart.Add(0,new KeyValuePair<ProductInStore, int>())
+        }
+
+    }
+}
---
+++ wsep192/Acceptance Tests/removeDiscountPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using src.ServiceLayer;
+
+namespace Acceptance_Tests
+{
+    [TestClass]
+    public class removeDiscountPolicy
+    {
+        ServiceLayer service;
+        String idOwner;
+        String ownerUser;
+        String passwordUser;
+    }
+}
